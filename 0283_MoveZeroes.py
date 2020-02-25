8ac0814a2fd013400dd24947fd0018e2cdf1b059
Jovana Nikolov <jovana@Vladas-MacBook-Pro-2.local>
2020-02-25 11:58:12 +0100

    final

--- 0283_MoveZeroes.py
+++ 0283_MoveZeroes.py
@@ -2,7 +2,7 @@ class Solution(object):
     def moveZeroes(self, nums):
         nonzeroi=len(nums)-1
         i=0
-        while i<nonzeroi-1:
+        while i<nonzeroi:
             if nums[i]!=0:
                 i+=1
             else:
@@ -10,4 +10,10 @@ class Solution(object):
                     nums[j]=nums[j+1]
                 nums[nonzeroi]=0
                 nonzeroi-=1
+
+sol=Solution()
+print('Enter the array:')
+a=[int(x) for x in input().split()]
+sol.moveZeroes(a)
+print(a)
                 
