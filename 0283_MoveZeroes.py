0cd8abdc310980ecfe79f814d22f8244c4b7eec9
Jovana Nikolov <jovana@Vladas-MacBook-Pro-2.local>
2020-02-28 16:38:18 +0100

    finalna verzija

--- 0283_MoveZeroes.py
+++ 0283_MoveZeroes.py
@@ -1,19 +1,15 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        sumOfZeroes=0
-        i=0
-        while i<len(nums):
+        numOfZeroes=0
+        for i in range(len(nums)):
             if nums[i]==0:
-                sumOfZeroes+=1
+                numOfZeroes+=1
             else:
-                nums[i-sumOfZeroes]=nums[i]
+                if numOfZeroes>0:
+                    nums[i-numOfZeroes]=nums[i]
+                    nums[i]=0
             i+=1
-        i=len(nums)-sumOfZeroes
-        while i<len(nums):
-            nums[i]=0
-            i+=1 
 
-sol=Solution()
 def test(inputList, outputList):
     inputListCopy = inputList[:]
 
@@ -32,7 +28,9 @@ def test(inputList, outputList):
     else:
         print('{0} - input = {1}, output = {2}, expected = {3}'.format(solOK, inputListCopy, inputList, outputList))
 
+test([0, 1, 2, 0, 3, 0, 4], [1, 2, 3, 4, 0, 0, 0])
 test([1, 2, 0, 3, 0, 4], [1, 2, 3, 4, 0, 0])
+test([1, 2, 0, 0, 0, 3, 0, 4], [1, 2, 3, 4, 0, 0, 0, 0])
 test([1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0, 0, 0, 0, 0,0,0])
 
                 
